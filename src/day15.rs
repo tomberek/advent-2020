8f18fc030b7ab68cd51c1a3e41a00737d328ba2c
Tom Bereknyei <tom@rebelliondefense.com>
2020-12-26 14:27:48 -0500

    use both hashmap and vec to store results for van eck

--- src/day15.rs
+++ src/day15.rs
@@ -6,14 +6,13 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 use rayon::prelude::*;
 use scan_fmt::{scan_fmt, scan_fmt_some};
-use std::collections::HashMap;
+use std::collections::{HashMap,hash_map::Entry};
 use std::collections::HashSet;
 
 use array2d::Array2D;
 
 use itertools::Itertools;
 use nohash_hasher::{IntMap, IntSet};
-use unroll::unroll_for_loops;
 
 #[aoc_generator(day15)]
 fn input_generator(inp: &str) -> Vec<usize> {
@@ -30,49 +29,16 @@ fn input_generator(inp: &str) -> Vec<usize> {
 fn part1(inp: &Vec<usize>) -> usize {
     return run(2020, inp);
 }
-
-#[unroll_for_loops]
 #[aoc(day15, part2)]
 fn part2(inp: &Vec<usize>) -> usize {
-    // https://stackoverflow.com/questions/53691012/is-there-any-way-to-allocate-a-standard-rust-array-directly-on-the-heap-skippin
-    // maybe ^^^ ?
-
-    const ITER : usize = 30_000_000;
-    let mut map = {
-        let mut map: Vec<u32> = Vec::with_capacity(ITER);
-        unsafe { map.set_len(ITER); };
-        let slice = map.into_boxed_slice();
-        // for i in &mut slice[..] {
-        //     *i = 0;
-        // }
-        let raw_slice = Box::into_raw(slice);
-        unsafe {
-                Box::from_raw(raw_slice as *mut [u32; ITER])
-        }
-    };
-
-    // let mut map_pre: Vec<u32> = vec![0; ITER];
-    // let mut map = map_pre.into_boxed_slice();
-    //let mut map: [u32;ITER] = [0; ITER];
-    let mut state = 0 as usize;
-    for i in 0..inp.len() {
-        let res = inp[i];
-        map[state] = i as u32;
-        state = res as usize;
-    }
-    unsafe {
-    for i in inp.len()..ITER {
-        let &v = map.get_unchecked(state);
-        let res = if v == 0 { 0 } else { i.unchecked_sub(v as usize) };
-        *map.get_unchecked_mut(state) = i as u32;
-        state = res;
-    }
-    }
-    return state;
+    return run(ITER, inp);
 }
 
+const ITER: usize = 30_000_000;
+const LIMIT : usize = ITER / 10;
 pub fn run(iter: usize, inp: &[usize]) -> usize {
-    let mut map: Vec<u32> = vec![0; iter];
+    let mut hmap: HashMap<u32,u32> = HashMap::new();
+    let mut map: Vec<u32> = vec![0; LIMIT];
     let mut state = 0 as usize;
     for i in 0..inp.len() {
         let res = inp[i];
@@ -80,10 +46,17 @@ pub fn run(iter: usize, inp: &[usize]) -> usize {
         state = res as usize;
     }
     for i in inp.len()..iter {
-        let v = map[state];
-        let res = if v == 0 { 0 } else { i - v as usize };
-        map[state] = i as u32;
-        state = res;
+        if state >= LIMIT {
+            match hmap.entry(state as u32) {
+                Entry::Occupied(mut v) => state = i -v.insert(i as u32) as usize,
+                Entry::Vacant(v) => { v.insert(i as u32); state = 0},
+            }
+        } else {
+            let v = map[state];
+            let res = if v == 0 { 0 } else { i - v as usize };
+            map[state] = i as u32;
+            state = res;
+        }
     }
     return state;
 }
